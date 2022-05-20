c99f7e1e22665a9c6fe1145b836cd27357ed4395
Lucas <Lucas@Lucas-PC>
2022-05-20 12:47:20 -0300

    Exemplo de JPA+Hibernate com relacionamentos entre Tabelas através de chaves estrangeiras.

--- AulaHibernate/src/main/java/Pessoa.java
+++ AulaHibernate/src/main/java/Pessoa.java
@@ -1,9 +1,12 @@
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Pessoa implements Serializable {
--- AulaHibernate/src/main/java/Principal.java
+++ AulaHibernate/src/main/java/Principal.java
@@ -5,28 +5,32 @@ import javax.persistence.Persistence;
 public class Principal {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa (null,"Lucas", "lucas@gmail.com","Professor");
-		Pessoa p2 = new Pessoa (null,"João", "joao@gmail.com","Aluno");
-		Pessoa p3 = new Pessoa (null,"Pedro", "pedro@gmail.com","Funcionário");
+		Pessoa p1 = new Pessoa(null, "Lucas", "lucas@gmail.com", "Professor");
+		Pessoa p2 = new Pessoa(null, "João", "joao@gmail.com", "Aluno");
+		Pessoa p3 = new Pessoa(null, "Pedro", "pedro@gmail.com", "Funcionário");
 		Pessoa p;
-		
-		//System.out.println(p1);
-		//System.out.println(p2);
-		//System.out.println(p3);
-		
+		// System.out.println(p1);
+		// System.out.println(p2);
+		// System.out.println(p3);
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaHibernate");
 		EntityManager em = emf.createEntityManager();
 
 		em.getTransaction().begin();
-		//em.persist(p1);
-		//em.persist(p2);
-		//em.persist(p3);
-
-		em.getTransaction().commit();
-		
-		p = em.find(Pessoa.class, 3);
+		p = em.find(Pessoa.class, 2);
 		System.out.println(p);
+		p.setNome("Robson");
+		// em.refresh(p);
+		// em.persist(p1);
+		// em.persist(p2);
+		// em.persist(p3);
+		em.getTransaction().commit();
+
+
 		
+
+		p = em.find(Pessoa.class, 2);
+		System.out.println(p);
 	}
 
 }
